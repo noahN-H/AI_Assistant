256bdb2183a1630280c879d7d3fec710c978fa6c
noahN-H <n.norrman.hijazi@gmail.com>
2026-08-29 00:08:10 +0100

    Stage 2 - Persistant Memory

--- testConnectionn.py
+++ testConnectionn.py
@@ -2,20 +2,40 @@ import os
 from anthropic import Anthropic
 from dotenv import load_dotenv
 
+
 chat = True
 history = []
 load_dotenv()
 client = Anthropic(
     api_key=os.environ.get("ANTHROPIC_API_KEY"),  # This is the default and can be omitted
 )
+
+persistantHistory = open(r"D:\Obsidian\Knowledge\history.md", "r")
+read_history = persistantHistory.read()
+   
 while chat:
     msg = input()
     if msg == "sleep":
         chat = False
+        exit_msg = client.messages.create(
+            max_tokens = 1024,
+            system = "review and output what you think is relevant in long term memory",
+            messages = history + [{"role": "user","content": "review and output what you think is relevant in long term memory"}], 
+            model = "claude-sonnet-5",
+        )  
+        
+        with open(r"D:\Obsidian\Knowledge\history.md", "a") as persistantHistory:
+            for i in exit_msg.content:
+                if i.type == "text":
+                    persistantHistory.write(i.text)
         break
+    
+    
     history.append({"role": "user","content": msg})
+    
     message = client.messages.create(
         max_tokens = 1024,
+        system = read_history,
         messages = history,
         model = "claude-sonnet-5",
     )
---
+++ testObsidian.py
@@ -0,0 +1,4 @@
+f  = open(r"D:\Obsidian\Knowledge\new_note.md", "a")
+f.write(f"{input()} \n")
+
+f.close()
